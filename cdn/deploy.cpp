dd1d17c4e75e0c219dd4d29c0df6a7e19eaf2fd0
PanCheng111 <pancheng@pku.edu.cn>
2017-03-22 16:10:16 +0800

    brutal force

---
+++ cdn/deploy.cpp
@@ -0,0 +1,118 @@
+#include "deploy.h"
+#include "maxflow.h"
+
+#include <stdio.h>
+#include <cstring>
+
+using namespace std;
+
+const int maxn = 1010;
+const int INIT_ANS_LEN = 10010;
+const int MAX_FLOW = 1000000000;
+
+vector<int> edge[maxn];
+vector<int> width[maxn];
+vector<int> cost[maxn];
+
+int N, Edge, Consumer, tot_consumes = 0;
+int cons[maxn], _cons[maxn];
+int snode[maxn], tnode[maxn], tneed[maxn];
+char *ans_str;
+int ans_str_len, cur_ans_len;
+int server_price;
+
+void build_edge(int x, int y, int w, int c) {
+	edge[x].push_back(y);
+	width[x].push_back(w);
+	cost[x].push_back(c);
+
+	edge[y].push_back(x);
+	width[y].push_back(w);
+	cost[y].push_back(c);	
+}
+
+void add_to_answer(char *word) {
+	int len = strlen(word);
+	if (ans_str_len + len > cur_ans_len) {
+		cur_ans_len *= 2;
+		char *tmp = new char[cur_ans_len];
+		memcpy(tmp, ans_str, sizeof(char) * ans_str_len);
+		delete ans_str;
+		ans_str = tmp;
+	}
+	strcat(ans_str, word);
+	ans_str_len += len;
+}
+
+void calc_max_flow(int* snode, int slen, int* tnode, int tlen) {
+	int S = N, T = N + 1;
+	MaxFlow *maxflow = new MaxFlow(N + 2, N, N + 1);
+	for (int i = 0; i < slen; i++) 
+		maxflow->connect(S, snode[i], MAX_FLOW, 0);
+	for (int i = 0; i < tlen; i++) 
+		maxflow->connect(tnode[i], T, tneed[tnode[i]], 0);
+	for (int i = 0; i < N; i++)
+		for (unsigned int j = 0; j < edge[i].size(); j++)
+			maxflow->connect(i, j, width[i][j], cost[i][j]);
+
+	int ans_flow = 0, ans_cost = 0;
+	maxflow->calc_result(ans_flow, ans_cost);
+	maxflow->find_route();
+
+	ans_str = new char[INIT_ANS_LEN];
+	cur_ans_len = INIT_ANS_LEN;
+
+	int route_cnt = maxflow->get_route_cnt();
+	sprintf(ans_str, "%d\n\n", route_cnt);
+
+	for (int i = 0; i < route_cnt; i++) {
+		int *route = NULL;
+		int route_len = 0;
+		maxflow->get_route(i, route, route_len);
+		for (int j = 1; j < route_len; j++) {
+			char num[10];
+			sprintf(num, "%d ", route[j]);
+			add_to_answer(num);
+		}
+		char end[10];
+		sprintf(end, "%d %d\n", _cons[route[route_len - 1]], route[0]);
+		add_to_answer(end);
+	}
+
+}
+
+//你要完成的功能总入口
+void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
+{
+	sscanf(topo[0], "%d %d %d", &N, &Edge, &Consumer);
+	sscanf(topo[2], "%d", &server_price);
+
+	int next = 0;
+	for (int i  = 4; i < line_num; i++) {
+		int x, y, w, c;
+		if (strcmp(topo[i], "\r\n") == 0) { next = i + 1; break; }
+		sscanf(topo[i], "%d %d %d %d", &x, &y, &w, &c);
+		build_edge(x, y, w, c);
+		build_edge(y, x, w, c);
+	}
+	for (int i = next; i < line_num; i++) {
+		int x, y, w;
+		sscanf(topo[i], "%d %d %d", &x, &y, &w);
+		cons[x] = y; _cons[y] = x;
+		tneed[y] = w;
+		tot_consumes += w;
+	}
+
+	for (int i = 0; i < Consumer; i++) {
+		tnode[i] = cons[i];
+		snode[i] = cons[i];
+	}
+
+	calc_max_flow(snode, Consumer, tnode, Consumer);
+	// 需要输出的内容
+	//char * topo_file = (char *)"17\n\n0 8 0 20\n21 8 0 20\n9 11 1 13\n21 22 2 20\n23 22 2 8\n1 3 3 11\n24 3 3 17\n27 3 3 26\n24 3 3 10\n18 17 4 11\n1 19 5 26\n1 16 6 15\n15 13 7 13\n4 5 8 18\n2 25 9 15\n0 7 10 10\n23 24 11 23";
+
+	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
+	write_result(ans_str, filename);
+
+}
---
+++ cdn/maxflow.h
@@ -0,0 +1,174 @@
+#ifndef __MAXFLOW_H__
+#define __MAXFLOW_H__
+#endif
+
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+class MaxFlow {
+
+public:
+	MaxFlow(int n, int s, int t) {
+		N = n;
+		S = s;
+		T = t;
+	}
+	~MaxFlow() {
+		for (int i = 0; i < N; i++) {
+			edge[i].clear();
+			flow[i].clear();
+			cost[i].clear();
+			edge_forward[i].clear();
+			edge_backward[i].clear();
+		}
+	}
+
+	void connect(int x, int y, int f, int c) {
+
+		edge[x].push_back(y);
+		flow[x].push_back(f);
+		cost[x].push_back(c);
+
+		edge[y].push_back(x);
+		flow[y].push_back(0);
+		cost[y].push_back(-c);
+
+		oppo[x].push_back(edge[y].size() - 1);
+		oppo[y].push_back(edge[x].size() - 1);
+
+		edge_forward[x].push_back(y);
+		edge_backward[x].push_back(edge[y].size() - 1);
+	}
+
+	void calc_result(int &ans_f, int &ans_c) {
+		ans_flow = 0;
+		ans_cost = 0;
+		while (find()) add_flow();
+		ans_f = ans_flow;
+		ans_c = ans_cost;
+	}
+
+	void find_route() {
+		route_cnt = 0;
+		stack[0] = S;
+ 		DFS(S, 1, 100000000);
+	}
+
+	int get_route_cnt() {
+		return route_cnt;
+	}
+
+	void get_route(int index, int * &route, int &len) {
+		route = route_head[index];
+		len = route_len[index];
+	}
+
+private:
+	static const int INF = 2147483647;
+	static const int maxn = 2010;
+	static const int max_route_cnt = 50010;
+
+	int N, S, T;
+	int ans_cost, ans_flow;
+	int route_cnt = 0;
+
+	vector<int> edge_forward[maxn];
+	vector<int> edge_backward[maxn];
+	vector<int> edge[maxn];
+	vector<int> flow[maxn];
+	vector<int> cost[maxn];
+	vector<int> oppo[maxn];
+	
+	bool use[maxn];
+	int pre[maxn];
+	int dis[maxn], frm[maxn];
+
+	int stack[maxn];
+	int *route_head[max_route_cnt];
+	int route_len[max_route_cnt];
+
+	bool find() {
+		memset(pre, 0, sizeof(pre));
+		memset(use, 0, sizeof(use));
+		queue<int> Queue;
+		Queue.push(S);
+		for (int i = 0; i < N; i++) 
+			dis[i] = INF;
+		dis[S] = 0;
+		while (!Queue.empty()) {
+			int x = Queue.front();
+			Queue.pop();
+			for (unsigned int i = 0; i < edge[x].size(); i++) {
+				int y = edge[x][i];
+				if (flow[x][i] == 0) continue;
+				if (cost[x][i] + dis[x] < dis[y]) {
+					dis[y] = cost[x][i] + dis[x];
+					pre[y] = x; frm[y] = i;
+					if (!use[y]) {
+						use[y] = true;
+						Queue.push(y);
+					}
+				}
+			}
+			use[x] = false;
+		}
+		return (dis[T] != INF);
+	}
+
+	void add_flow() {
+		int x = T, y, min = INF, i;
+		while (x != S) {
+			y = pre[x];
+			i = frm[x];
+			if (flow[y][i] < min) min = flow[y][i];
+			x = y;
+		}
+
+		x = T;
+		while (x != S) {
+			y = pre[x];
+			i = frm[x];
+			flow[y][i] -= min;
+			flow[x][oppo[y][i]] += min;
+			x = y;
+		}
+		ans_flow += min;
+		ans_cost += min * dis[T];
+	}
+
+	int miner(int a, int b) {
+		if (a < b) return a;
+		return b;
+	}
+
+	int DFS(int x, int len, int rest) {
+		if (x == T) {
+			int *route = new int[len];
+			for (int i = 0; i < len - 1; i++) 
+				route[i] = stack[i];
+			route[0] = rest;
+			route_head[route_cnt ++] = route;
+			route_len[route_cnt - 1] = len - 1;
+			return rest;
+		}
+		int used = 0;
+		for (unsigned int i = 0; i < edge_forward[x].size(); i++) 
+			if (edge_forward[x][i] >= 0) {
+				int y = edge_forward[x][i];
+				int j = edge_backward[x][i];
+				if (flow[y][j] == 0) continue;
+				stack[len] = y;
+				int ret = DFS(y, len + 1, miner(rest, flow[y][j])); 
+				flow[x][i] += ret;
+				flow[y][j] -= ret;
+				rest -= ret;
+				used += ret;
+			}
+		return used;
+
+	}
+
+};
+

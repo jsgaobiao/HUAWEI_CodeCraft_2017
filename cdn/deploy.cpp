60e32d073fd377841bf5a3612c96c5145d25352b
jsgaobiao <jsgaobiao@pku.edu.cn>
2017-03-23 23:21:26 +0800

    fix some bug

--- cdn/deploy.cpp
+++ cdn/deploy.cpp
@@ -127,6 +127,7 @@ void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
 
     for (int i = 0; i < Consumer; i++) {
         tnode[i] = cons[i];
+      //  snode[i] = cons[i];
 	}
 
     for (int i = 0; i < N; i ++)
@@ -134,7 +135,7 @@ void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
             snode[serverCnt ++] = i;
         }
 
-	calc_max_flow(snode, Consumer, tnode, Consumer);
+    calc_max_flow(snode, serverCnt, tnode, Consumer);
 	// 需要输出的内容
 	//char * topo_file = (char *)"17\n\n0 8 0 20\n21 8 0 20\n9 11 1 13\n21 22 2 20\n23 22 2 8\n1 3 3 11\n24 3 3 17\n27 3 3 26\n24 3 3 10\n18 17 4 11\n1 19 5 26\n1 16 6 15\n15 13 7 13\n4 5 8 18\n2 25 9 15\n0 7 10 10\n23 24 11 23";
 
